41a04ed81079fd9ed0ba0c29de0489f9ea81ab6a
willnicholson <152422235+willnicholson@users.noreply.github.com>
2023-12-29 10:35:27 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,80 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/BenzProfilePicture.jpg"
+about_me = "I'm Will Nicholson. I am a first year Industrial Engineering major studying here at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/william-nicholson-6190a62a2/"
+my_github_url = "https://github.com/willnicholson"
+my_email_address = "wwnicholson4@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': "Spring 26'",
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "ENGL 1102", "ISyE 2027", "ISyE 3030"], 
+    "names":["Intro to CS", "English Composition II", "Probability with Applications", "Basic Statistical Methods"], 
+    "semester_taken":["1st", "1st", "1st", "2nd"],
+    "skills":["Couldn't tell you what recursion is", "I can now understand English!", "There are 6 sides to a die", "Still can't predict the outcome of the election :("],
+    }
+experience_data = {
+    "Leader in Band": (["- Served on the Roswell High School Leadership team for 3 years",
+                                                                          "- Became woodwind captain and drum major", "- Led band to first place finish for first time in 5 years"],"images/DrumMajorChampPicture.jpg"),
+    "Proficient in Probability":(["- Got 6th place at the Georgia Tech International Probability Competition",
+                                                           "- Plans to become a TA in ISyE 2027"],"images/ProbabilityAchievementPicture.jpg"),
+    "Future Intern":(["- Plans to complete an internship this upcoming Summer of 2024"],"images/InternPicture.jpg")
+
+}
+
+projects_data = {
+    "Personal Profile": "Created this personal profile while taking CS1301",
+    "Fantasy Football Pages": "Created info page on NFL receivers and a miniquiz on fantasy football from scratch",
+    "Fantasy Football Draft Guide Web App": "Created a Web App through Streamlit on a brief guide to how to draft based on draft position, draft strategy, and commitment to fantasy fooball",
+}
+
+programming_data = {
+    "Python": 95,
+    "Java": 30,
+    "C": 5,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Beginner",
+    "Spanish": "Almost Fluent",
+}
+leadership_data = {
+    "Band Leadership": (["- Served as woodwind captain and drum major in final two years at Roswell"],"images/DrumMajorPicture.jpg"),
+}
+activity_data={
+    "United Sound": ["- Taught special needs students how to play instruments", 
+            "- Held a concert featuring the students playing pieces all together"],
+    "Help a Hornet": ["- Help underpriveledged children by buying them presents for Christmas"],
+    "Veterinarian Volunteer": ["- Volunteered at PetVet Inc., a veterinary clinic in Cumming, GA",
+            "- Helped spay and neuter stray cats and dogs and shadowed surgeon in clinic"]
+}
